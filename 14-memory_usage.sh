b854d37e5a6b63daf357a83b5664b72b0bc08882
suresh <sureshkmr123789@gmail.com>
2024-05-27 21:13:01 +0530

    shell practice

--- 14-memory_usage.sh
+++ 14-memory_usage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 RAM_USAGE=$(top | awk -F " " '{print $11F}')
-THRESHOLD_VALUE=0.01
+
 
 
 while IFS= read -r line
 do
+    THRESHOLD_VALUE=0.01
     if [ $line -ge $THRESHOLD_VALUE ]
     then
-        echo "$RAM_USAGE is more the the $THRESHOLD_VALUE"
+        echo "$line is more the the $THRESHOLD_VALUE"
     fi
 done <<< $RAM_USAGE
