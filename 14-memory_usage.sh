dd186ca3856d8a4cd8feddbe05fd785506c72fed
suresh <sureshkmr123789@gmail.com>
2024-05-27 21:28:09 +0530

    shell practice

--- 14-memory_usage.sh
+++ 14-memory_usage.sh
@@ -6,7 +6,7 @@ THRESHOLD_VALUE=0.01
 
 while IFS= read -r line
 do
-    RAM_USAGE=$(echo "$line | awk -F " " '{print $11F}'")
+    RAM_USAGE=$(echo $line | awk -F " " '{print $11F}')
     if [ $RAM_USAGE -ge $THRESHOLD_VALUE ]
     then
         echo "$line is more the the $THRESHOLD_VALUE"
